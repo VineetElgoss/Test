13f9590f2646b3fd4bf8bf67f700abc3e408c73c
VineetElgoss <vineet.elgoss@gmail.com>
2025-03-17 10:05:56 +0530

    first commit

---
+++ future.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main(){
+  var list =  [1,2,3,4,54,5];
+
+
+  list.add(10);
+  print(list);
+
+  var str = <String>{
+
+  };
+  // String? name;
+    print('Enter your name');
+    // name = stdin.readLineSync();
+    var name = stdin.readLineSync();
+    print('your is $name');
+    print('hello');
+
+//   if(name == null){
+//     print('true');
+//   }
+//   var image = {
+//   1: ['saturn'],
+//   'url': '//path/to/saturn.jpg',
+// };
+// print(image[1]);
+}
